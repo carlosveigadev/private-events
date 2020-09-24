5a6470dccb0532740f68d82127c7e9f21e8f54b2
Sanad <s_wj_88@hotmail.com>
2020-09-25 01:30:30 +0300

    Generate user and event scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :events
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'events#index'
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
 end
