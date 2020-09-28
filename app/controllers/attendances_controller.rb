3d7a873b8ae192a697bff934c0d1daaf113d7f47
wrakc <carlosveiga1618@gmail.com>
2020-09-28 19:29:24 -0300

    add resources to routes

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,8 +1,10 @@
 class AttendancesController < ApplicationController
   def create
-    user = User.find(:user_id)
-    event = Event.find(:event_id)
+    user = User.find(current_user.id)
+    ////////////////////////////////////////event = Event.find_by(id: 1)
+
     event.attendees << user
+
     if event.save
       redirect_to event_path(event)
     else
@@ -11,24 +13,7 @@ class AttendancesController < ApplicationController
     end
   end
 
-  def show
-    @attendance = Attendance.all
-  end
-
-  def new
-  end
-
   def destroy
   end
 
-  private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_attendance
-    @attendance = Attendance.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  def attendance_params
-    params.require(:attendance).permit(:user_id, :event_id)
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'attendances/new'
   get 'attendances/destroy'
   resources :sessions, only: [:new, :create, :destroy]
+  resources :attendances, only: [:create, :destroy]
   resources :events
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -11,5 +12,5 @@ Rails.application.routes.draw do
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   post 'signup', to: 'users#create'
-  post 'attendance', to: 'attendances#create'
+  # post 'attendance', to: 'attendances#create'
 end
