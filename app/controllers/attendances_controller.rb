68b83040471b2afd8c996e7ea57965f2950b866a
wrakc <carlosveiga1618@gmail.com>
2020-09-30 14:57:42 -0300

    add layouts to events edit and new page

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -4,7 +4,8 @@ class AttendancesController < ApplicationController
     @event.attendees << current_user
 
     if @event.save
-      redirect_to @event
+      redirect_to user_path(current_user)
+      flash[:alert] = "Nice, you are checked in to the event!" 
     else
       flash[:alert] = "Unable to send invite"
       redirect_to @event
